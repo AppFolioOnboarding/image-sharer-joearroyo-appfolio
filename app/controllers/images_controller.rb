07394bfcb177cad58bd1ddbc1c187dee844d604e
Joe Arroyo <joseph.arroyo@appfolio.com>
2020-10-12 10:44:22 -0700

    Validate image urls and show image when validation passes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,23 @@
 class ImagesController < ApplicationController
+  def show
+    @image = Image.find(params[:id])
+  end
+  
   def new
+    @image = Image.new
   end
 
   def create
-    render plain: params[:image].inspect
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to @image
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def image_params
+    params.require(:image).permit(:url)
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,19 @@
 class Image < ApplicationRecord
+  validates :url, presence: true, 
+            format: { with: /.+\.(png|jpg|gif|tiff)\Z/i, message: "Invalid image extension" }
+  validate :url_must_be_formatted_properly
+
+  def url_must_be_formatted_properly
+    error_label = :invalid_url_format
+    error_msg = "Invalid url format"
+    begin
+      uri = URI.parse(url)
+      if !uri.is_a?(URI::HTTP) || uri.host.nil?
+        errors.add(:url, error_label, message: error_msg)
+      end
+    rescue URI::InvalidURIError #we don't want an exception to be thrown, only for validation to fail
+      errors.add(:url, error_label, message: error_msg)
+    end
+  end
+
 end
