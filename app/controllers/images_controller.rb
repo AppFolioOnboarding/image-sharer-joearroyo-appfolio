45a51d0b5ac2a66fb0331d2b8ffa134b1c728f51
Joe Arroyo <joseph.arroyo@appfolio.com>
2020-10-13 13:18:41 -0700

    fix rubocop issues with code formatting

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ class ImagesController < ApplicationController
   def show
     @image = Image.find(params[:id])
   end
-  
+
   def new
     @image = Image.new
   end
@@ -17,6 +17,7 @@ class ImagesController < ApplicationController
   end
 
   private
+
   def image_params
     params.require(:image).permit(:url)
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,15 +12,15 @@ module ActiveSupport
 end
 
 module ErrorHelper
-  
-  #Finds out whether an error exists on this model object that is specific to a given validation name
-  #Rather than testing whether an attribute is simply valid, this can test whether an attribute is 
-  #valid in the context of a specfic validation test.
+  # Finds out whether an error exists on this model object that is specific to a given validation name
+  # Rather than testing whether an attribute is simply valid, this can test whether an attribute is
+  # valid in the context of a specfic validation test.
 
-  #Is there a better way to do this in ruby?
-  #There is a rails function on errors called 'added?' but it only works for _some_ validations
-  #It fails to succeed when testing for a format validation because the format detail contains two entries in its hash, unlike the others
-  def ErrorHelper.has_error_for_validation(model, attribute, validation_error)
-    model.errors.details[attribute].any? {|detail| detail[:error] == validation_error}
+  # Is there a better way to do this in ruby?
+  # There is a rails function on errors called 'added?' but it only works for _some_ validations
+  # It fails to succeed when testing for a format validation
+  # because the format detail contains two entries in its hash, unlike the others
+  def self.error_for_validation(model, attribute, validation_error)
+    model.errors.details[attribute].any? { |detail| detail[:error] == validation_error }
   end
 end
