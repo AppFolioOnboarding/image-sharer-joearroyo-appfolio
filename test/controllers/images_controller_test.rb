de2eb6148674a8fc03980d56c29963fa322406ad
Joe Arroyo <joseph.arroyo@appfolio.com>
2020-10-13 15:58:03 -0700

    CR fix: added unnecessary comments from image controller tests

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -16,12 +16,10 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should create image url' do
-    # attempt to add a new valid url image, the number of images should change due to successfully adding it
     assert_difference('Image.count') do
       post images_url, params: { image: { url: 'http://test.png' } }
     end
 
-    # upon success, we should be redirected to the show page for the image that was just created
     assert_redirected_to image_path(Image.last)
   end
 end
