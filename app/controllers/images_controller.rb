d04a7589368f662d88c0e4e6eb5d40c8d87497fa
Joe Arroyo <joseph.arroyo@appfolio.com>
2020-10-15 09:47:59 -0700

    ja - add fallback to display all images if tag is invalid in the tag search

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
   def index
-    @images = Image.order(created_at: :desc)
+    @images = all_images_ordered
   end
 
   def show
@@ -21,10 +21,17 @@ class ImagesController < ApplicationController
   end
 
   def tag_search
-    @images = Image.tagged_with(params[:tag])
+    @images = Image.tagged_with(params[:tag]).order(created_at: :desc)
+
+    # if the tag search is unsuccesfull we'll currently fall back to displaying all images
+    @images = all_images_ordered if @images.empty?
   end
 
   private
+
+  def all_images_ordered
+    Image.order(created_at: :desc)
+  end
 
   def image_params
     params.require(:image).permit(:url, :tag_list)
